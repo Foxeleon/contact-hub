aae93e2472b84d0e69a3252f3f03785a33819864
Foxeleon <andreywirz@gmail.com>
2025-11-06 15:41:30 +0100

    devops: Containerize application with Docker and Nginx Reverse Proxy

--- backend/go.mod
+++ backend/go.mod
@@ -1,6 +1,6 @@
 module contact-hub/backend
 
-go 1.25.1
+go 1.25.4
 
 require (
 	github.com/go-chi/chi/v5 v5.2.3
--- backend/main.go
+++ backend/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
+	"contact-hub/backend/internal/parser"
 	"fmt"
 	"log"
-	"contact-hub/backend/internal/parser"
 )
 
 func main() {
-    fmt.Println("Launching the service Contact Hub...")
+	fmt.Println("Launching the service Contact Hub...")
 
-    // Example of calling the parser stub
-    persons, err := parser.LoadPersons("./data")
-    if err != nil {
-    	log.Fatalf("Error when uploading data: %v", err)
-    }
+	// Example of calling the parser stub
+	persons, err := parser.LoadPersons("./data")
+	if err != nil {
+		log.Fatalf("Error when uploading data: %v", err)
+	}
 
-    log.Printf("The parser stub has been successfully invoked. Uploaded %d records.", len(persons))
-    // TODO: Add storage initialization and HTTP server startup.
+	log.Printf("The parser stub has been successfully invoked. Uploaded %d records.", len(persons))
+	// TODO: Add storage initialization and HTTP server startup.
 }
